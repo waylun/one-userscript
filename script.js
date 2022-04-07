05369c2ee890f0aa3e415a365af76f6cb6840bc6
lun <waylunly@gmail.com>
2022-04-07 18:21:41 +0800

    better

--- script.js
+++ script.js
@@ -9,107 +9,91 @@ $(document).ready(function() {
     }
 
     if (pf_url.includes('apps.shopify.com/one/reviews')) {
-        fetchReviewScript()
+        if (pf_ONE?.fetch_review) {
+            fetchReviewScript()
+        }
     }
 
     function fetchReviewScript() {
-        // create button
-        const ffdiv = document.createElement('div')
-        ffdiv.id = 'ffdiv'
-        ffdiv.style = 'position:fixed; bottom:0; right:200px; z-index:2147483646;'
-        document.body.appendChild(ffdiv)
-        var ffbutton = document.createElement('ffbutton')
-        ffbutton.innerHTML = 'FETCH REVIEWS'
-        ffbutton.id = 'one-fetch-reviews'
-        ffbutton.style.padding = '10px'
-        ffbutton.style.opacity = '0.8'
-        const theDiv = document.getElementById('ffdiv')
-        theDiv.append(ffbutton)
-        // listening
-        document.getElementById('one-fetch-reviews').addEventListener("click", () => {
-            ffbutton.style.display = 'none'
-            // start fetching
-            const fxdiv = document.createElement('div')
-            fxdiv.id = 'fxdiv'
-            fxdiv.style = 'position:fixed; bottom:0; right:0; z-index:9999;'
-            document.body.appendChild(fxdiv)
-            var button = document.createElement('button')
-            const theDiv = document.getElementById('fxdiv')
-            theDiv.append(button)
-            button.id = 'one+'
-            button.style = 'background: black;padding: 15px 20px;color: white; border-radius: 5px;'
-            button.innerHTML = 'One+'
+        const fxdiv_0 = document.createElement('div')
+        fxdiv_0.id = 'fxdiv_0'
+        fxdiv_0.style = 'position:fixed; bottom:0; right:0; z-index:9999;'
+        document.body.appendChild(fxdiv_0)
+        var button_0 = document.createElement('button_0')
+        const theDiv = document.getElementById('fxdiv_0')
+        theDiv.append(button_0)
+        button_0.id = 'one+'
+        button_0.style = 'background: black;padding: 15px 20px;color: white; border-radius: 5px;'
+        button_0.innerHTML = 'One+'
 
-            document.getElementById(button.id).addEventListener("click", function() {
-                let data = JSON.parse(sessionStorage.getItem("weeee"))
-                var tt = arrayToCSV(data)
-                let csvContent = "data:text/csv;charset=utf-8," + tt
-                var encodedUri = encodeURI(csvContent);
-                var link = document.createElement("a");
-                link.setAttribute("href", encodedUri);
-                link.setAttribute("download", "one_review.csv");
-                document.body.appendChild(link);
-                link.click();
-            });
+        document.getElementById(button_0.id).addEventListener("click", function() {
+            let data = JSON.parse(sessionStorage.getItem("weeee"))
+            var tt = arrayToCSV(data)
+            let csvContent = "data:text/csv;charset=utf-8," + tt
+            var encodedUri = encodeURI(csvContent);
+            var link = document.createElement("a");
+            link.setAttribute("href", encodedUri);
+            link.setAttribute("download", "one_review.csv");
+            document.body.appendChild(link);
+            link.click();
+        });
 
-            expendText();
-            var el = document.querySelectorAll(".review-listing .review-content .truncate-content-copy p")
-            var ee = document.querySelectorAll(".review-listing .ui-star-rating")
-            var et = document.querySelectorAll(".review-listing .review-listing-header__text")
-            var ep = document.querySelectorAll(".review-listing .review-metadata__item:nth-child(2) .review-metadata__item-value")
-            var eg = document.querySelector('.search-pagination__tag')
+        expendText();
+        var el = document.querySelectorAll(".review-listing .review-content .truncate-content-copy p")
+        var ee = document.querySelectorAll(".review-listing .ui-star-rating")
+        var et = document.querySelectorAll(".review-listing .review-listing-header__text")
+        var ep = document.querySelectorAll(".review-listing .review-metadata__item:nth-child(2) .review-metadata__item-value")
+        var eg = document.querySelector('.search-pagination__tag')
 
-            var go
-            try {
-                go = JSON.parse(sessionStorage.getItem("weeee")) || []
-            } catch (e) {
-                go = []
-            }
+        var go
+        try {
+            go = JSON.parse(sessionStorage.getItem("weeee")) || []
+        } catch (e) {
+            go = []
+        }
 
-            for (let i = 0; i < ee.length; i++) {
-                go.push({
-                    review: el[i].innerHTML.replace(/(?:&nbsp;|<br>|\n)/g, ' ').trim(),
-                    rating: ee[i].dataset.rating,
-                    title: et[i].innerHTML.trim(),
-                    posted: ep[i].innerHTML.trim(),
-                    page: eg.innerHTML
-                })
-            }
+        for (let i = 0; i < ee.length; i++) {
+            go.push({
+                review: el[i].innerHTML.replace(/(?:&nbsp;|<br>|\n)/g, ' ').trim(),
+                rating: ee[i].dataset.rating,
+                title: et[i].innerHTML.trim(),
+                posted: ep[i].innerHTML.trim(),
+                page: eg.innerHTML
+            })
+        }
 
-            sessionStorage.setItem("weeee", JSON.stringify(go));
-            const pre = document.querySelector('.search-pagination__previous-page-text')
-            const next = document.querySelector('.search-pagination__next-page-text')
+        sessionStorage.setItem("weeee", JSON.stringify(go));
+        const pre = document.querySelector('.search-pagination__previous-page-text')
+        const next = document.querySelector('.search-pagination__next-page-text')
 
-            if (next) {
-                setTimeout(() => {
-                    next.click()
-                }, 1000)
-            }
-            if (!pre && !next) {
-                setTimeout(() => {
-                    window.location.reload();
-                }, 60000)
-            }
+        if (next) {
+            setTimeout(() => {
+                next.click()
+            }, 1000)
+        }
+        if (!pre && !next) {
+            setTimeout(() => {
+                window.location.reload();
+            }, 60000)
+        }
 
-            function expendText() {
-                var mm = document.querySelectorAll(".review-listing .truncate-content-toggle--show")
-                for (let i = 0; i < mm.length; i++) {
-                    if (mm[i].innerText === 'Show full review') {
-                        mm[i].click()
-                    }
+        function expendText() {
+            var mm = document.querySelectorAll(".review-listing .truncate-content-toggle--show")
+            for (let i = 0; i < mm.length; i++) {
+                if (mm[i].innerText === 'Show full review') {
+                    mm[i].click()
                 }
             }
+        }
 
-            function arrayToCSV(objArray) {
-                const array = typeof objArray !== 'object' ? JSON.parse(objArray) : objArray;
-                let str = `${Object.keys(array[0]).map(value => `"${value}"`).join(",")}` + '\r\n';
-                return array.reduce((str, next) => {
-                    str += `${Object.values(next).map(value => `"${value}"`.replace(/,/g, ' ').replace(/'/g, ' ').replace(/"/g, ' ')).join(",")}` + '\r\n';
-                    return str;
-                }, str);
-            }
-
-        });
+        function arrayToCSV(objArray) {
+            const array = typeof objArray !== 'object' ? JSON.parse(objArray) : objArray;
+            let str = `${Object.keys(array[0]).map(value => `"${value}"`).join(",")}` + '\r\n';
+            return array.reduce((str, next) => {
+                str += `${Object.values(next).map(value => `"${value}"`.replace(/,/g, ' ').replace(/'/g, ' ').replace(/"/g, ' ')).join(",")}` + '\r\n';
+                return str;
+            }, str);
+        }
     }
 
     function screenshotScript() {
@@ -121,17 +105,17 @@ $(document).ready(function() {
         const FW_height = pf_ONE?.FW_height || 976
 
         // create button
-        const fxdiv = document.createElement('div')
-        fxdiv.id = 'fxdiv'
-        fxdiv.style = 'position:fixed; bottom:0; right:200px; z-index:2147483646;'
-        document.body.appendChild(fxdiv)
-        var button = document.createElement('button')
-        button.innerHTML = 'CAPTURE SCREENSHOT'
-        button.id = 'capture'
-        button.style.padding = '10px'
-        button.style.opacity = '0.8'
-        const theDiv = document.getElementById('fxdiv')
-        theDiv.append(button)
+        const fxdiv_1 = document.createElement('div')
+        fxdiv_1.id = 'fxdiv_1'
+        fxdiv_1.style = 'position:fixed; bottom:0; right:200px; z-index:2147483646;'
+        document.body.appendChild(fxdiv_1)
+        var button_1 = document.createElement('button_1')
+        button_1.innerHTML = 'CAPTURE SCREENSHOT'
+        button_1.id = 'capture'
+        button_1.style.padding = '10px'
+        button_1.style.opacity = '0.8'
+        const theDiv = document.getElementById('fxdiv_1')
+        theDiv.append(button_1)
 
         const ob = new MutationObserver((_muta, me) => {
             let el = document.querySelector('.proof-factor-cb-overlay')
